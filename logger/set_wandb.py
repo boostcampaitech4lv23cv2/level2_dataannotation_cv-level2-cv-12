254513a429d1e96ddef66ba2ad6809d14d856af7
yongwkim <kyw9404@gmail.com>
2022-12-12 02:14:49 +0000

    feat : set wandb setting with config file

---
+++ logger/set_wandb.py
@@ -0,0 +1,10 @@
+import wandb
+import json
+import os
+
+def wandb_init(args):
+    with open('conf.json') as f:
+        config = json.load(f)
+    wandb.init(project=config['wandb_project'], name=args.experiment_name, entity=config['wandb_entity'])
+    wandb.config.update(args)
+    os.makedirs(os.path.join(args.model_dir, args.experiment_name), exist_ok=True)
