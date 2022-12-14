46dbaa9cb993b0eb03617138bb5ef80b1d7e6c4c
wowns1484 <wowns1484@naver.com>
2022-12-14 07:15:44 +0000

    add multiprocessing

--- train.py
+++ train.py
@@ -25,6 +25,9 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 from albumentations.augmentations.geometric.resize import LongestMaxSize
 
+import multiprocessing
+from multiprocessing import Pool
+
 def parse_args():
     parser = ArgumentParser()
 
@@ -61,7 +64,7 @@ def parse_args():
 
     return args
 
-def do_training(args,model):
+def do_training(args,model,process_cnt,process_pool):
     print("\n##### TRAINING #####")
     dataset = SceneTextDataset(args.train_dir, split='train', image_size=args.image_size, crop_size=args.input_size)
     dataset = EASTDataset(dataset)
@@ -86,44 +89,44 @@ def do_training(args,model):
 
     for epoch in range(1,args.max_epoch+1):
         print('\n ### epoch {} ###'.format(epoch))
-        epoch_loss, start = 0, time.time()
-        train_dict = defaultdict(int)
-        model.train()
-        with tqdm(total=num_batches) as pbar:
-            for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
-                #pbar.set_description('[Epoch {}]'.format(epoch + 1))
+        # epoch_loss, start = 0, time.time()
+        # train_dict = defaultdict(int)
+        # model.train()
+        # with tqdm(total=num_batches) as pbar:
+        #     for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
+        #         #pbar.set_description('[Epoch {}]'.format(epoch + 1))
 
-                loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
+        #         loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+        #         optimizer.zero_grad()
+        #         loss.backward()
+        #         optimizer.step()
 
-                loss_val = loss.item()
-                epoch_loss += loss_val
+        #         loss_val = loss.item()
+        #         epoch_loss += loss_val
 
-                pbar.update(1)
-                tmp_dict = {
-                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                    'IoU loss': extra_info['iou_loss']
-                }
-                pbar.set_postfix(tmp_dict)
+        #         pbar.update(1)
+        #         tmp_dict = {
+        #             'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
+        #             'IoU loss': extra_info['iou_loss']
+        #         }
+        #         pbar.set_postfix(tmp_dict)
 
-                train_dict['train_total_loss'] += loss.item() / len(train_loader)
-                train_dict['train_cls_loss'] += extra_info['cls_loss'] / len(train_loader)
-                train_dict['train_angle_loss'] += extra_info['angle_loss'] / len(train_loader)
-                train_dict['train_iou_loss'] += extra_info['iou_loss'] / len(train_loader)
+        #         train_dict['train_total_loss'] += loss.item() / len(train_loader)
+        #         train_dict['train_cls_loss'] += extra_info['cls_loss'] / len(train_loader)
+        #         train_dict['train_angle_loss'] += extra_info['angle_loss'] / len(train_loader)
+        #         train_dict['train_iou_loss'] += extra_info['iou_loss'] / len(train_loader)
 
-        scheduler.step()
-        train_dict['epoch'] = epoch
-        train_dict['learning_rate'] = optimizer.param_groups[0]['lr']
-        wandb.log(train_dict)
+        # scheduler.step()
+        # train_dict['epoch'] = epoch
+        # train_dict['learning_rate'] = optimizer.param_groups[0]['lr']
+        # wandb.log(train_dict)
 
-        print('[train] loss: {:.4f} | Elapsed time: {}'.format(
-            epoch_loss / num_batches, timedelta(seconds=time.time() - start)))
+        # print('[train] loss: {:.4f} | Elapsed time: {}'.format(
+        #     epoch_loss / num_batches, timedelta(seconds=time.time() - start)))
 
         ### validation ###
         start = time.time()
-        res_dict = do_valdation(model=model, loader=val_loader, gt_bboxes_dict=val_gt_dict, transcriptions_dict=val_illegibility_dict, input_size=args.val_input_size)
+        res_dict = do_valdation(model=model, loader=val_loader, gt_bboxes_dict=val_gt_dict, transcriptions_dict=val_illegibility_dict, input_size=args.val_input_size, process_cnt=process_cnt, process_pool=process_pool)
         val_dict = res_dict['total']
         val_dict['epoch'] = epoch
         wandb.log(val_dict)
@@ -205,13 +208,15 @@ def do_warmup(args, model):
     
 def main(args):
     set_seed(args.seed)
-
+    process_cnt = multiprocessing.cpu_count()
+    process_pool = Pool(process_cnt)
+    
     model = EAST()
     model.to(args.device)
 
     if args.warmup:
         do_warmup(args, model)
-    do_training(args, model)
+    do_training(args, model, process_cnt, process_pool)
 
 
 if __name__ == '__main__':
